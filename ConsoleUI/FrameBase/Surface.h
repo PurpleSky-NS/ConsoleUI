b9d1d9d400573df8a8e5c6b16e22c830e86fd59d
Purple <wpq2225232027@163.com>
2019-11-21 23:54:12 +0800

    修改Surface部分虚函数

--- ConsoleUI/FrameBase/Surface.h
+++ ConsoleUI/FrameBase/Surface.h
@@ -15,9 +15,6 @@ public:
 	/*刷新界面*/
 	void Refresh();
 
-	/*传递事件字符*/
-	void PostEvent(char inputText);
-
 	/*加入的顺序即为显示的优先级顺序*/
 	void Add(UIComponent* com);
 
@@ -30,11 +27,14 @@ public:
 	/*全部删除，会释放内存*/
 	void Clear();
 
+	/*传递事件字符*/
+	virtual void PostEvent(char inputText);
+
 	/*界面被添加到界面栈中回调*/
-	void OnStart() {}
+	virtual void OnStart() {}
 
 	/*界面从界面栈中删除时回调*/
-	void OnClose() {}
+	virtual void OnClose() {}
 
 private:
 	UIComponent* m_onEventCom = nullptr;
